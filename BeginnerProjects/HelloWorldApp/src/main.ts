c2097033fc3f8d1de7ba590e0c716ed4fe0dae02
sinkukumar <sudosinku@gmail.com>
2024-08-29 19:17:01 +0530

    Update hello world app

---
+++ BeginnerProjects/HelloWorldApp/src/main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'root-app',
+  standalone: true,
+  template: `<h1>Hello World!</h1>`,
+})
+export class DemoComponent {
+}
+
+bootstrapApplication(DemoComponent);
